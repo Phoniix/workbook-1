28e766778286b73a46a5a536da5b75555b176e0f
Sean <seanasb52058@gmail.com>
2025-04-11 14:29:32 -0500

    Completed Mod3 - Exercise 3 - Basic Calculator

--- PayrollCalculator/src/main/java/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/PayrollCalculator.java
@@ -1,22 +1,71 @@
 import java.util.Scanner;
 
 public class PayrollCalculator {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in); // Introduce Scanner
 
     public static void main (String[] args) {
+        PayrollCalculator.entirePayrollCalc();
+    }
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------- //
+
+// Methods for the PayrollCalculator
+    // Method 1
+
+    static double grossPayCalcMethod (double payRate, double hoursWorked) {
+        double grossRegPay = payRate * hoursWorked; // Hours times payrate
+
+        System.out.println("Based on " + hoursWorked + " hours. At " + payRate + " per/hour, is equal to: $" + grossRegPay);
+        return grossRegPay;
+    }
+
+//--------------------------------------------------------------------------------------------------------------------------- //
+    // Method 2
+
+    static double overTimeCalcMethod (double payRate, double hoursWorked) {
+        double regularHours = 40; //Regular hours = 40, since this method only applies IF there is overtime
+        double regularPay = payRate; //regular payrate with no changes
+
+        double overTimeHours = hoursWorked - regularHours; //separates how many hours are overtime vs regular
+        double overTimePay = 1.5 * payRate; //1.5 times the payrate to multiply with overtime hours
+
+
+        double grossOTPay = 0;
+
+        grossOTPay = (regularHours * regularPay) + (overTimeHours * overTimePay);
+        System.out.println("Based on " + hoursWorked + " hours. At " + payRate + " per/hour, is equal to: $" + grossOTPay);
+
+        return grossOTPay;
+
+
+    }
+
+//--------------------------------------------------------------------------------------------------------------------------- //
+    //Method 3
+
+    static double entirePayrollCalc() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(" ");
         System.out.println("Enter your name: ");
-        String employeeName = scanner.nextLine();
+        String employeeName = scanner.nextLine(); //Employee Name Input
 
         System.out.println(" ");
         System.out.println(" Enter your hours worked: ");
-        double hoursWorked = Double.parseDouble(scanner.nextLine());
+        double hoursWorked = Double.parseDouble(scanner.nextLine()); //Employee's jours worked input
 
         System.out.println(" ");
         System.out.println("Enter your pay rate per/hour: ");
-        double payRate = Double.parseDouble(scanner.nextLine());
+        double payRate = Double.parseDouble(scanner.nextLine()); //Employee's pay rate input
 
         double grossPay = 0.0;
 
@@ -24,11 +73,12 @@ public class PayrollCalculator {
         System.out.println("Hello, " + employeeName + "!");
 
         if (hoursWorked > 40) {
-            grossPay = (40 * payRate) + ((hoursWorked - 40) * (1.5 * payRate));
-            System.out.println("Based on " + hoursWorked + " hours. At " + payRate + " per/hour, is equal to: $" + grossPay);
+            grossPay = PayrollCalculator.overTimeCalcMethod(payRate, hoursWorked);
+
         } else {
-            grossPay = payRate * hoursWorked;
-            System.out.println("Based on " + hoursWorked + " hours. At " + payRate + " per/hour, is equal to: $" + grossPay);
+            grossPay = PayrollCalculator.grossPayCalcMethod(payRate, hoursWorked);
         }
+
+        return grossPay;
     }
 }
